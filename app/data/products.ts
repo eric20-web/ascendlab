340afd3e5ac5f3e8bf429140ba6d14b52dd0a91b
Eric <ericmani597@gmail.com>
2026-08-27 00:48:49 +0100

    Temporarily set hoodie price to £1

--- app/data/products.ts
+++ app/data/products.ts
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 1,
     name: "ASCENDLAB Black Hoodie",
-    price: 44.99,
+    price: 1.00,
     image: "/images/ascend-hoodie.jpeg",
     description: "Premium heavyweight streetwear hoodie.",
     sizes: ["XS", "S", "M", "L", "XL", "XXL"],
