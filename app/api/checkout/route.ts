d1d92010f614d2d14b940f4c6558d424de09d69d
Eric <ericmani597@gmail.com>
2026-09-13 19:55:03 +0100

    Enable Stripe dynamic payment methods

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -34,7 +34,6 @@ export async function POST(request: Request) {
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-      payment_method_types: ["card"],
       line_items: lineItems,
 
       success_url:
